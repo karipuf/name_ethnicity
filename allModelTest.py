16e1377e22940f3ece7555948ba5ccdcffa5db99
Wei Lee Woon <wlwoon@gmail.com>
2017-12-06 14:17:04 +0400

    Added in command line arguments for allModelTest.py

--- allModelTest.py
+++ allModelTest.py
@@ -1,8 +1,23 @@
+from argparse import ArgumentParser
+
+ap=ArgumentParser()
+ap.add_argument('-i',help='Input glob expression for meta files to be tested... default is all in current directory (be sure to use quotes for e.g. "*.meta")')
+parsed=ap.parse_args()
+if parsed.i==None:
+    inGlob="*.meta"
+else:
+    inGlob=parsed.i
+
 from glob import glob
 from itertools import chain
 import re,os,sys
 
-models=[re.compile('(.+)\.meta').findall(tmp)[0] for tmp in glob('currentBest_*.meta')]
+models=[re.compile('(.+)\.meta').findall(tmp)[0] for tmp in glob(inGlob)]
+
+print("Testing the following models:")
+print('\n'.join(models))
+sys.stdout.flush()
+
 cmds_a=['python test_accuracy.py -v -m '+tmp+' -i testSets/PsychologyEth.pkl 2> /dev/null' for tmp in models]
 cmds_b=['python test_accuracy.py -v -m '+tmp+' 2> /dev/null' for tmp in models]
 cmds=list(chain(*zip(cmds_a,cmds_b)))
